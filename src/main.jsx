01a43673bb646c628b58f61daa28e366a3eb7e29
Richard Hamstra <51700261+hamstera73@users.noreply.github.com>
2026-03-20 18:08:05 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// Unregister any existing service workers to prevent stale caching
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(reg => reg.unregister())
+  })
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
